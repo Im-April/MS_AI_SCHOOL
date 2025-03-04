a8418adbd289d6c33fdbca236c967556e658a1dd
April <ghwon4640@gmail.com>
2025-03-04 11:00:17 +0900

    HTML 강의 내용

---
+++ HTML/00_test.js
@@ -0,0 +1,9 @@
+let element_h1 = document.querySelector("h1");
+element_h1.onclick = function () {
+  element_h1.style.backgroundColor = "black";
+};
+
+const btnStart = document.querySelector("button");
+btnStart.onclick = function () {
+  alert("start");
+};
